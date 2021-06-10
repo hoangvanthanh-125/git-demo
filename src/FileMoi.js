b520858d713e6badfdeb4b09be99d3ef0c504c90
hoangthanh <thanhthanh125521@gmail.com>
2021-06-10 16:26:23 +0700

    them file moi

---
+++ src/FileMoi.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+FileMoi.propTypes = {
+  
+};
+
+function FileMoi(props) {
+  return (
+    <div>
+      File moi tao
+    </div>
+  );
+}
+
+export default FileMoi;
